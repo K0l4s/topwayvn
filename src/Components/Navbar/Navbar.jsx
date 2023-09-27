21f101ba8dbb78ad2d7a5128bc448c389c9f19ea
Trung Kien <21110223@student.hcmute.edu.vn>
2023-09-28 01:18:18 +0700

    Hi

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
         <div className="navbar">
             <Button onClick={handleChangeLanguage("en")}>English</Button>
             <Button onClick={handleChangeLanguage("vi")}>Tiếng Việt</Button>
+            
             <img src="https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-6/335372298_1286959945188947_1228093645418539727_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=mZez1jhf0f4AX96aiBa&_nc_ht=scontent.fsgn13-2.fna&_nc_e2o=f&oh=00_AfAbMiYLyvFfWkIXpNjGkV48J_vExzVOc1uoKW8TKrvefw&oe=65176BC7" 
             alt="logo" onClick={()=> navigate("/")}/>
             <div className="group">
