70406d9ed72da0027779f04cf33f3d02a410c6b2
taovietduc193 <95725645+taovietduc193@users.noreply.github.com>
2023-09-28 01:53:22 +0700

    Add

--- src/Pages/SanPham/TatCa/Tatca.jsx
+++ src/Pages/SanPham/TatCa/Tatca.jsx
@@ -1,46 +1,31 @@
 import React from 'react'
 import ViewSanPham from '../../../Components/ViewSanPham/ViewSanPham'
 import './Tatca.css'
+import { useTranslation } from 'react-i18next'
 const Tatca = () => {
+  const [t,i18n] = useTranslation("global");
   return (
     <div className="tatCa">
       <div className="productList">
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
-        <ViewSanPham name="San pham 1"
-          src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/382383342_1385378652392363_1479235805160386814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=JM5W8LIKfYMAX_8pX5s&_nc_ht=scontent.fsgn8-3.fna&_nc_e2o=f&oh=00_AfCoOCEF7cksRD8eVVzKL9yBGoiZSWayptThOVTsGqX45Q&oe=6518CE68" />
+        <ViewSanPham name={t("product.product_1.name")} src={t("product.product_1.ImageUrl")} />
+        <ViewSanPham name={t("product.product_2.name")} src={t("product.product_2.ImageUrl")} />
+        <ViewSanPham name={t("product.product_3.name")} src={t("product.product_3.ImageUrl")} />
+        <ViewSanPham name={t("product.product_4.name")} src={t("product.product_4.ImageUrl")} />
+        <ViewSanPham name={t("product.product_5.name")} src={t("product.product_5.ImageUrl")} />
+        <ViewSanPham name={t("product.product_6.name")} src={t("product.product_6.ImageUrl")} />
+        <ViewSanPham name={t("product.product_7.name")} src={t("product.product_7.ImageUrl")} />
+        <ViewSanPham name={t("product.product_8.name")} src={t("product.product_8.ImageUrl")} />
+        <ViewSanPham name={t("product.product_9.name")} src={t("product.product_9.ImageUrl")} />
+        <ViewSanPham name={t("product.product_10.name")} src={t("product.product_10.ImageUrl")} />
+        <ViewSanPham name={t("product.product_11.name")} src={t("product.product_11.ImageUrl")} />
+        <ViewSanPham name={t("product.product_12.name")} src={t("product.product_12.ImageUrl")} />
+        <ViewSanPham name={t("product.product_13.name")} src={t("product.product_13.ImageUrl")} />
+        <ViewSanPham name={t("product.product_14.name")} src={t("product.product_14.ImageUrl")} />
+        <ViewSanPham name={t("product.product_15.name")} src={t("product.product_15.ImageUrl")} />
+        <ViewSanPham name={t("product.product_16.name")} src={t("product.product_16.ImageUrl")} />
+        <ViewSanPham name={t("product.product_17.name")} src={t("product.product_17.ImageUrl")} />
+        <ViewSanPham name={t("product.product_18.name")} src={t("product.product_18.ImageUrl")} />
+        <ViewSanPham name={t("product.product_19.name")} src={t("product.product_19.ImageUrl")} /> 
       </div>
     </div>
   )
